7f7c9d20083457e30eed0380102e3d5278e2c49c
Abogeerick <abogeerick@gmail.com>
2023-09-04 12:01:45 +0300

    Imported func construct

--- lib/models.py
+++ lib/models.py
@@ -2,6 +2,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import func 
 
 Base = declarative_base()
 
