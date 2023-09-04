6807d58bc35c6dc9b3ff11b883d9cc71cd118f8f
Abogeerick <abogeerick@gmail.com>
2023-09-04 15:06:02 +0300

    Added data to mydatabase.db

--- lib/seed.py
+++ lib/seed.py
@@ -1,13 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Restaurant, Customer, Review
-
-
-
+import random
 
 def seed_database():
+    # Define your SQLite database URL here
+    DATABASE_URL = "sqlite:///mydatabase.db"
+
     # Create a database engine
-    engine = create_engine("sqlite:///mydatabase.db")  
+    engine = create_engine(DATABASE_URL)
+
     # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -19,11 +21,14 @@ def seed_database():
     customer1 = Customer(first_name="John", last_name="Doe")
     customer2 = Customer(first_name="Jane", last_name="Smith")
 
-    review1 = Review(rating=4, restaurant_id=restaurant1.id, customer_id=customer1.id)
-    review2 = Review(rating=5, restaurant_id=restaurant2.id, customer_id=customer2.id)
-
-    # Add instances to the session
-    session.add_all([restaurant1, restaurant2, customer1, customer2, review1, review2])
+    # Generate and insert 50 random reviews
+    for _ in range(50):
+        rating = random.randint(1, 5)  # Generate a random rating between 1 and 5
+        # Choose a random restaurant and customer for the review
+        restaurant = random.choice([restaurant1, restaurant2])
+        customer = random.choice([customer1, customer2])
+        review = Review(rating=rating, restaurant_id=restaurant.id, customer_id=customer.id)
+        session.add(review)
 
     # Commit the changes to the database
     session.commit()
